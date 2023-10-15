1429986cfabd14a86123c2e0f328aca3c1cc3c2c
Filipe André <flipeandre12@gmail.com>
2023-10-15 13:00:24 -0300

    feat: tratamento de exceções

--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -41,57 +41,81 @@ public class Hotel : IHotel
 
     public void NovaSuite()
     {
-        Console.WriteLine("Qual o tipo da suite?");
-        string tipoSuite = Console.ReadLine();
-        Console.WriteLine("Qual a capacidade ?");
-        int CapacidadeDeHospedes = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Qual o valor por dia ?: Exemplo 20,00");
-        decimal valorDiariaSuite = Convert.ToDecimal(Console.ReadLine());
-        Suite suite = new(tipoSuite, CapacidadeDeHospedes, valorDiariaSuite);
-        SuitesDisponiveis.Add(suite);
-        Console.WriteLine("Suite adicionada com sucesso !");
+        try
+        {
+            Console.WriteLine("Qual o tipo da suite?");
+            string tipoSuite = Console.ReadLine();
+            Console.WriteLine("Qual a capacidade ?");
+            int CapacidadeDeHospedes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qual o valor por dia ?: Exemplo 20,00");
+            decimal valorDiariaSuite = Convert.ToDecimal(Console.ReadLine());
+            Suite suite = new(tipoSuite, CapacidadeDeHospedes, valorDiariaSuite);
+            SuitesDisponiveis.Add(suite);
+            Console.WriteLine("Suite adicionada com sucesso !");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Exceção de argumento: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine($"Exceção genérica: {ex.Message}");
+        }
     }
 
 
     public void NovaReserva()
     {
-        List<Pessoa> hospedes = new();
-        Console.WriteLine("Quantas pessoas utilizarão a suite?");
-        int quantidadeHospedes = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Quantas Dias ficarão na suite?");
-        int quantidadeDiasNaSuite = Convert.ToInt32(Console.ReadLine());
-        if (quantidadeHospedes > 0)
+        try
         {
-            for (int count = 0; count < quantidadeHospedes; count++)
+            List<Pessoa> hospedes = new();
+            Console.WriteLine("Quantas pessoas utilizarão a suite?");
+            int quantidadeHospedes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Quantas Dias ficarão na suite?");
+            int quantidadeDiasNaSuite = Convert.ToInt32(Console.ReadLine());
+            if (quantidadeHospedes > 0)
             {
-                Console.WriteLine($"Digite o nome do hospede Nº{count + 1}: ");
-                string nome = Console.ReadLine();
-                Console.WriteLine($"Digite o sobrenome do hospede Nº{count + 1}: ");
-                string sobrenome = Console.ReadLine();
-                Pessoa pessoa = new(nome: nome, sobrenome: sobrenome);
-                hospedes.Add(pessoa);
-            }
-            Suite suiteDisponivel = SuitesDisponiveis.Find(
-                                                suite
-                                                => suite.Capacidade
-                                                >= quantidadeHospedes);
-            if (suiteDisponivel != null)
-            {
-                IReserva<List<Pessoa>, Suite> reserva = new Reserva(quantidadeDiasNaSuite);
-                reserva.CadastrarSuite(suiteDisponivel);
-                reserva.CadastrarHospedes(hospedes);
-                ReservasDoHotel.Add((Reserva)reserva);
-                Console.WriteLine("Reserva criada !");
+                for (int count = 0; count < quantidadeHospedes; count++)
+                {
+                    Console.WriteLine($"Digite o nome do hospede Nº{count + 1}: ");
+                    string nome = Console.ReadLine();
+                    Console.WriteLine($"Digite o sobrenome do hospede Nº{count + 1}: ");
+                    string sobrenome = Console.ReadLine();
+                    Pessoa pessoa = new(nome: nome, sobrenome: sobrenome);
+                    hospedes.Add(pessoa);
+                }
+                Suite suiteDisponivel = SuitesDisponiveis.Find(
+                                                    suite
+                                                    => suite.Capacidade
+                                                    >= quantidadeHospedes);
+                if (suiteDisponivel != null)
+                {
+                    IReserva<List<Pessoa>, Suite> reserva = new Reserva(quantidadeDiasNaSuite);
+                    reserva.CadastrarSuite(suiteDisponivel);
+                    reserva.CadastrarHospedes(hospedes);
+                    ReservasDoHotel.Add((Reserva)reserva);
+                    Console.WriteLine("Reserva criada !");
+                }
+                else
+                {
+                    Console.WriteLine("Não foi possível encontrar " +
+                                        "Uma suite disponível !");
+                }
             }
             else
             {
-                Console.WriteLine("Não foi possível encontrar " +
-                                    "Uma suite disponível !");
+                Console.WriteLine("Quantidade de hospedes inválidos !");
             }
         }
-        else
+        catch (ArgumentException ex)
         {
-            Console.WriteLine("Quantidade de hospedes inválidos !");
+            Console.WriteLine($"Exceção de argumento: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine($"Exceção genérica: {ex.Message}");
         }
     }
 
